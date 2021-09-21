c14be1c560912a30c70a257b7670bd7db4d65b40
Alberto Schmalbach <albert2550@hotmail.com>
2021-09-20 21:11:42 -0500

    Nuevos archivos y capetas

---
+++ Clase_1/funciones.js
@@ -0,0 +1,5 @@
+document.querySelector('#html').addEventListener('click', (() => alert('Soy la pestaña HTML')));
+
+document.querySelector('#css').addEventListener('click', (() => alert('Soy la pestaña CSS')));
+
+document.querySelector('#js').addEventListener('click', (() => alert('Soy la pestaña JavaScript')));
